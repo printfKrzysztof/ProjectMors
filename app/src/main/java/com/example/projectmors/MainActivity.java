f0e499131e917e62545be6a69c358c0fdc6c912d
Krzyś Bortnowski <krzysztofbortnowski@outlook.com>
2022-01-12 13:00:32 +0100

    Szkielet obsługi latarki
    Mała poprawka

--- app/src/main/java/com/example/projectmors/MainActivity.java
+++ app/src/main/java/com/example/projectmors/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.projectmors;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button stoppopup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         EditText tekst = findViewById(R.id.I_Tekst);
         EditText mors = findViewById(R.id.I_Mors);
 
@@ -60,11 +71,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void Glownyprogram(View adoz)
+    public void OnClick(View adoz)
     {
-
         //String kodmorsa = mors.getText().toString();
-
+        createNewDialog();
     }
 
     /* Funkacja odpowiedzialna za tlumaczenie*/
@@ -74,6 +84,26 @@ public class MainActivity extends AppCompatActivity {
         //chmurka = Toast.makeText(this,"Wprowadzono złe dane",Toast.LENGTH_LONG);
         //chmurka.show();
         return wpisane;
+    }
+
+    public void createNewDialog()
+    {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View Popup = getLayoutInflater().inflate(R.layout.popup, null);
+
+        stoppopup = (Button) Popup.findViewById(R.id.stoper);
+
+        dialogBuilder.setView(Popup);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        stoppopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
     }
 
 }
